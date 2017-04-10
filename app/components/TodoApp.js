d4062e179e896e01420d807ccfbf99fe592a5c00
kollie <zikollie@gmail.com>
2017-04-10 12:10:42 +0000

    Add search functionality

--- app/components/TodoApp.js
+++ app/components/TodoApp.js
@@ -3,12 +3,15 @@
 import React from 'react'
 import TodoList from 'TodoList'
 import AddTodo from 'AddTodo'
+import TodoSearch from 'TodoSearch'
 
 
 export default class TodoApp extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
+      showCompleted: false,
+      searchText: '',
       todos: [
         {
           id: 1,
@@ -29,16 +32,25 @@ export default class TodoApp extends React.Component {
       ]
     }
     this.handleTodo = this.handleTodo.bind(this)
+    this.handleSearch = this.handleSearch.bind(this)
   }
 
   handleTodo (text) {
     alert('new todo: ' + text)
   }
 
+  handleSearch (showCompleted, searchText) {
+    this.setState({
+      showCompleted: showCompleted,
+      searchText: searchText.toLowerCase()
+    })
+  }
+
   render () {
     let {todos} = this.state
     return (
       <div>
+        <TodoSearch onSearch={this.handleSearch}/>
         <TodoList todos={todos}/>
         <AddTodo onAddTodo={this.handleTodo}/>
       </div>
---
+++ app/components/TodoSearch.js
@@ -0,0 +1,33 @@
+'user strict'
+
+import React from 'react'
+
+export default class TodoSearch extends React.Component {
+  constructor (props) {
+    super(props)
+    this.handleSearch = this.handleSearch.bind(this)
+  }
+
+  handleSearch () {
+    let showCompleted = this.refs.showCompleted.checked
+    let searchText = this.refs.searchText.value
+
+    this.props.onSearch(showCompleted, searchText)
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <input type='search' ref='searchText' placeholder='Search Todos' onChange={this.handlSearch}/>
+        </div>
+        <div>
+          <label>
+            <input type='checkbox' ref='showCompleted' onChange={this.handleSearch}/>
+            Show completed Todos
+          </label>
+        </div>
+      </div>
+    )
+  }
+}
