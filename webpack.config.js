b776dd4be7e02d820fd69047433c1923011e538d
kollie <zikollie@gmail.com>
2017-06-06 15:22:49 +0000

    Add actions and action tests

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,8 @@ module.exports = {
      './app/api'
    ],
     alias: {
-      applicationStyles: 'app/styles/app.scss'
+      applicationStyles: 'app/styles/app.scss',
+      actions: 'app/actions/action.js'
     },
     extensions: ['*', '.js']
   },
